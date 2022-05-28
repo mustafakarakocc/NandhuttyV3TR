32711348f65cbd52077ed831b3f9358eb5f60599
EkeLachin <104994662+EkeLachin@users.noreply.github.com>
2022-05-28 10:47:42 +0300

    Update stickerTelegram.js

--- plugins/stickerTelegram.js
+++ plugins/stickerTelegram.js
@@ -2,10 +2,10 @@ const fetch = require('node-fetch')
 const { sticker5 } = require('../lib/sticker')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use:\n${usedPrefix + command} <url>\n\nExample :\n${usedPrefix + command} https://t.me/addstickers/namapack`
+    if (!args[0]) throw `Şöyle yap:\n${usedPrefix + command} <url>\n\nÖrnek :\n${usedPrefix + command} https://t.me/addstickers/namapack`
     if (!args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) throw `url salah`
     let packName = args[0].replace('https://t.me/addstickers/', '')
-    let res = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(packName)}`, { method: 'GET', headers: { 'User-Agent': 'GoogleBot' } })
+    let res = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(packName)}`, { method: 'EDİNMEK', headers: { 'User-Agent': 'GoogleBot' } })
     if (!res.ok) throw eror
     let json = await res.json()
     m.reply(`*Total stickers:* ${json.result.stickers.length}
@@ -18,7 +18,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         await conn.sendFile(m.chat, stiker, '', '', 0, 0, { asSticker: true })
         await conn.delay(1500)
     }
-    m.reply('_*Finished*_')
+    m.reply('_*BİTTİ!*_')
 }
 handler.help = ['stikertele <url>']
 handler.tags = ['sticker']
