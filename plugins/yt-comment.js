f7eee1224d8bc3b498933c93162b5b9d3ff24776
EkeLachin <104994662+EkeLachin@users.noreply.github.com>
2022-05-28 10:29:12 +0300

    Update yt-comment.js

--- plugins/yt-comment.js
+++ plugins/yt-comment.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Use:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} this is a comment`
+  if (!text) throw `Use:\n${usedPrefix + command} <text>\n\nÖrnek:\n${usedPrefix + command} bu bir yorum`
   conn.sendFile(m.chat, API('https://some-random-api.ml', '/canvas/youtube-comment', {
     avatar: await conn.getProfilePicture(m.sender).catch(_ => ''),
     comment: text,
