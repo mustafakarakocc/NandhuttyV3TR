0dcc3209816c26db18dd06c17a376d033852044f
EkeLachin <104994662+EkeLachin@users.noreply.github.com>
2022-05-28 10:45:01 +0300

    Update stickerLine.js

--- plugins/stickerLine.js
+++ plugins/stickerLine.js
@@ -3,7 +3,7 @@ const { sticker5 } = require('../lib/sticker')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-    if (!args[0]) throw `Use :\n${usedPrefix + command} <url>\n\nExample :\n${usedPrefix + command} https://store.line.me/stickershop/product/8149770`
+    if (!args[0]) throw `Şöyle yap :\n${usedPrefix + command} <url>\n\nÖrnek :\n${usedPrefix + command} https://store.line.me/stickershop/product/8149770`
     if (!args[0].match(/(https:\/\/store.line.me\/stickershop\/product\/.*)/gi)) throw `wrong url`
 
     let res = await fetch(global.API('zeks', '/api/linesticker', { link: args[0] }, 'apikey'))
@@ -12,7 +12,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!json.status) throw json
     let hasil = json.sticker.map((v, i) => `${i + 1}. ${v}`).join('\n')
     m.reply(`*${json.title}*
-*Estimated complete:* ${json.sticker.length * 1.5} second
+*Tahmini tamamlandı:* ${json.sticker.length * 1.5} saniye
     `.trim())
 
     for (let i of json.sticker) {
@@ -20,7 +20,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         await conn.sendFile(m.chat, stiker, '', '', m, 0, { asSticker: true })
         await conn.delay(1500)
     }
-    m.reply('_*Finished*_')
+    m.reply('_*BİTTİ!*_')
 
 }
 handler.help = ['stickerline <url>']
