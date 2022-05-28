b539106095e1879906f4099e37d0099ee51661d9
EkeLachin <104994662+EkeLachin@users.noreply.github.com>
2022-05-28 10:34:31 +0300

    Update toimg2.js

--- plugins/toimg2.js
+++ plugins/toimg2.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   if (/webp/.test(mime)) {
     out = await webp2png(media)
   }
-  await conn.sendFile(m.chat, out, 'out.png', 'ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', m, false, { thumbnail: out })
+  await conn.sendFile(m.chat, out, 'out.png', 'Laçin Eke YouTube', m, false, { thumbnail: out })
 }
 handler.help = ['toimg2']
 handler.tags = ['sticker']
