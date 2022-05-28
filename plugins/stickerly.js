44ad9880371e349292212a38d53c9793ab8a4e2d
EkeLachin <104994662+EkeLachin@users.noreply.github.com>
2022-05-28 10:52:40 +0300

    Update stickerly.js

--- plugins/stickerly.js
+++ plugins/stickerly.js
@@ -2,14 +2,14 @@ const fetch = require('node-fetch')
 const { sticker5 } = require('../lib/sticker')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Use :\n${usedPrefix + command} <text>\n\nExample :\n${usedPrefix + command} spongebob`
+    if (!text) throw `Şöyle yap :\n${usedPrefix + command} <text>\n\nÖrnek :\n${usedPrefix + command} YouTube`
     let res = await fetch(API('zeks', '/api/searchsticker', { q: text }, 'apikey'))
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
     let hasil = json.sticker.map((v, i) => `${i + 1}. ${v}`).join('\n')
     m.reply(`*${json.title}*
-*Estimated complete:* ${json.sticker.length * 1.5} second
+*Tahmini tamamlandı:* ${json.sticker.length * 1.5} saniye
 `.trim())
 
     for (let i of json.sticker) {
@@ -17,7 +17,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         await conn.sendFile(m.chat, stiker, '', '', m, 0, { asSticker: true })
         await conn.delay(1500)
     }
-    m.reply('_*Finished*_')
+    m.reply('_*BİTTİ!*_')
 
 }
 handler.help = ['stickerly <text>']
